c3d0bea8b03e539eae991e99b37767e62a7a4299
Samir Otiv <samir@ubuntu.members.linode.com>
2017-03-03 11:48:46 +0000

    Linux Setup separated

---
+++ build.sh
@@ -0,0 +1,1 @@
+bash home/.vim/bundle/install.sh
--- setup-linux.sh
+++ setup-linux.sh
@@ -1,4 +1,6 @@
 sudo apt-get install build-essential cmake -y
 sudo apt-get install python-dev python3-dev -y
 
-bash home/.vim/bundle/install.sh
+./build.sh
+
+./copy.sh
